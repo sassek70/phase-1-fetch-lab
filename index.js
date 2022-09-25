0f315adae1366a1086a3a3972b1ebd891c0577b0
Kevin Sasse <sassek70@gmail.com>
2022-09-25 17:07:54 -0400

    finshed lab

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then(function (returnedBooks) {
+    return renderBooks(returnedBooks)
+  })
 }
 
 function renderBooks(books) {
@@ -9,9 +12,9 @@ function renderBooks(books) {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
-  });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
-});
+  });
