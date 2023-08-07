5086b9f3883f5d57710a54ed462c720e631c18ee
Kevin Sasse <sassek70@gmail.com>
2023-08-07 00:05:55 -0400

    commit tree aaee799f260764a50a8c714048cd8a9709d07f3b

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-function fetchBooks() {
-  return fetch("https://anapioficeandfire.com/api/books")
-  .then((resp) => resp.json())
-  .then(function (returnedBooks) {
-    return renderBooks(returnedBooks)
-  })
-}
+// function fetchBooks() {
+//   return fetch("https://anapioficeandfire.com/api/books")
+//   .then((resp) => resp.json())
+//   .then(function (returnedBooks) {
+//     return renderBooks(returnedBooks)
+//   })
+// }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-    });
-}
+// function renderBooks(books) {
+//   const main = document.querySelector('main');
+//   books.forEach(book => {
+//     const h2 = document.createElement('h2');
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//     });
+// }
 
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
